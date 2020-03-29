7a4ff558f9b9c0a54bd7e7f63526d0ae8c6ed4e6
Arsen Klyuev <aklioue1@jhu.edu>
2020-03-28 21:14:35 -0400

    Made code more modularized, changed plots to all go on one plot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LoadBalancerSimulator
+
+A program that simulates packets being sent from clients through load-balancers to servers, allowing the testing of various load balancing techniques. 
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ num_load_balancers = 2
 num_servers = 4
 processing_time = 0.005 # time per packet
 
+assignment_methods = ["RandomAssignment", "ConsistentHashing", "PowersOfTwoNoMemory", "PowersOfTwoWithMemory"]
+
 #class defining a client/host
 class Client:
 	def __init__(self, address):
@@ -19,11 +21,11 @@ class LoadBalancer:
 		self.id = address
 		self.connection_table = {}
 
-	def assign_server_random(self, packet):
+	def assign_server_random(self, packet, _):
 		packet_id = packet.clientid
 		return random.randint(0, num_servers - 1)
 
-	def assign_server_hashing(self, packet):
+	def assign_server_hashing(self, packet, _):
 		return hash(str(packet.clientid)) % num_servers
 
 	def assign_server_power_of_2_choices_no_memory(self, packet, servers):
@@ -69,6 +71,9 @@ class Server:
 	def add_packet(self, packet):
 		self.packet_history.append(packet)
 
+	def clear_packets(self):
+		self.packet_history.clear()
+
 	#calculate the amount of time until the queue is theoretically free
 	def get_load(self, current_time):
 		if len(self.packet_history) == 0:
@@ -109,39 +114,90 @@ class Packet:
 	def __repr__(self):
 		return "Packet from client: " + str(self.clientid) + " @time: " + str(round(self.time_sent,3))
 
-# Initialization Steps
-packets = []
-for i in range(num_clients):
-	for j in range(packets_per_client):
-		packet = Packet(i, random.random())
-		packets.append(packet)
-packets.sort(key=lambda x: x.time_sent, reverse=False)
+def run_plotter(servers, assignment_method):
+	# Post-Simulation Analysis
+	for server in servers:
+		#print(server)
+		times = []
+		loads = []
+		for t in [x/100 for x in range(1, 100)]:
+			load = server.get_load(t)
+			# print("Load at time", t, "is", round(load, 3))
+			times.append(t)
+			loads.append(load)
 
-load_balancers = []
-for i in range(num_load_balancers):
-	load_balancer = LoadBalancer(i)
-	load_balancers.append(load_balancer)
+		
+		plt.plot(np.array([x for x in times]), np.array([y for y in loads]), label = server.id) # , s=5
+		plt.xlabel('Time')
+		plt.ylabel('Load (Time til finish)')
+		plt.title('Load vs. Time for Servers')
 
-servers = []
-for i in range(num_servers):
-	server = Server(i)
-	servers.append(server)
+	plt.savefig('plots/' + assignment_method + '/LoadVsTimeForServers.png')
+	plt.clf()
 
-# Main Simulation Processing Loop
-for packet in packets:
-	lb_id = packet.clientid % num_load_balancers
-	load_balancer = load_balancers[lb_id]
-	# server_id = load_balancer.assign_server_random(packet) # No per-flow consistency :(
-	# server_id = load_balancer.assign_server_hashing(packet) # Per-flow consistency :)
-	# server_id = load_balancer.assign_server_power_of_2_choices_no_memory(packet, servers) # No Per-flow consistency :( + Congestion control :)
-	server_id = load_balancer.assign_server_power_of_2_choices_with_memory(packet, servers) # Per-flow consistency :) + Congestion control :) + Local memory :)
-	server = servers[server_id]
-	server.add_packet(packet)
-	print("Load Balancer " + str(lb_id) + " sent packet from client " + str(packet.clientid) + " to server " + str(server_id))
+def run_consistency_check(servers):
+	perFlowConsistent = True
+	for server in servers:
+		for otherServer in servers:
+			if server.id != otherServer.id:
+				for packet in server.packet_history:
+					if packet.clientid in [pckt.clientid for pckt in otherServer.packet_history]:
+						perFlowConsistent = False
+						break
+	if perFlowConsistent:
+		print("Per-Flow Consistency Maintained")
+	else:
+		print("Per-Flow Consistency Not Maintained")
 
+
+def run_simulation(assignment_method):
+	print("running simulation for " + assignment_method)
+
+	# Initialization Steps
+	packets = []
+	for i in range(num_clients):
+		for j in range(packets_per_client):
+			packet = Packet(i, random.random())
+			packets.append(packet)
+	packets.sort(key=lambda x: x.time_sent, reverse=False)
+
+	load_balancers = []
+	for i in range(num_load_balancers):
+		load_balancer = LoadBalancer(i)
+		load_balancers.append(load_balancer)
+
+	servers = []
+	for i in range(num_servers):
+		server = Server(i)
+		servers.append(server)
+
+	# Main Simulation Processing Loop
+	for packet in packets:
+		lb_id = packet.clientid % num_load_balancers
+		load_balancer = load_balancers[lb_id]
+		switcher = {
+			"RandomAssignment": load_balancer.assign_server_random,  # No per-flow consistency :(
+			"ConsistentHashing": load_balancer.assign_server_hashing, # Per-flow consistency :)
+			"PowersOfTwoNoMemory": load_balancer.assign_server_power_of_2_choices_no_memory, # No Per-flow consistency :( + Congestion control :)
+			"PowersOfTwoWithMemory": load_balancer.assign_server_power_of_2_choices_with_memory # Per-flow consistency :) + Congestion control :) 
+		}
+		func = switcher.get(assignment_method, lambda: "Invalid assignment method")
+		server_id = func(packet, servers)
+		server = servers[server_id]
+		server.add_packet(packet)
+		#print("Load Balancer " + str(lb_id) + " sent packet from client " + str(packet.clientid) + " to server " + str(server_id))
+
+	run_plotter(servers, assignment_method)
+	run_consistency_check(servers)
+
+
+for method in assignment_methods:
+	run_simulation(method)
+
+'''
 # Post-Simulation Analysis
 for server in servers:
-	print(server)
+	#print(server)
 	times = []
 	loads = []
 	for t in [x/100 for x in range(1, 100)]:
@@ -150,13 +206,26 @@ for server in servers:
 		times.append(t)
 		loads.append(load)
 	
-	plt.plot(np.array([x for x in times]), np.array([y for y in loads])) # , s=5
+	#for making individual plots:
+	#plt.title('Load vs. Time for Server ' + str(server.id))
+	#plt.savefig('plots/3PowersOfTwoWithMemory/LoadVsTimeForServer' + str(server.id) + '.png')
+	#plt.clf()
+	
+	#ax.plot(np.array([x for x in times]), np.array([y for y in loads]), '-b', label=server.id)
+
+	
+	plt.plot(np.array([x for x in times]), np.array([y for y in loads]), label = server.id) # , s=5
 	plt.xlabel('Time')
 	plt.ylabel('Load (Time til finish)')
-	plt.title('Load vs. Time for Server ' + str(server.id))
-	plt.savefig('plots/LoadVsTimeForServer' + str(server.id) + '.png')
-	plt.clf()
+	plt.title('Load vs. Time for Servers')
+'''
+	
 
+#ax.axis('equal')
+#leg = ax.legend()
+#plt.savefig('plots/RandomAssignment/LoadVsTimeForServers.png')
+
+'''
 # Per-Flow Consistency Check:
 perFlowConsistent = True
 for server in servers:
@@ -170,5 +239,6 @@ if perFlowConsistent:
 	print("Per-Flow Consistency Maintained")
 else:
 	print("Per-Flow Consistency Not Maintained")
+'''
 
 
